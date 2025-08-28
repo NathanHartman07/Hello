e8fd69f9666462bce455b08e35918a3ba430fd13
Nathan Hartman <nhartman5@huskers.unl.edu>
2025-08-28 11:43:42 -0500

    Hello stuff

---
+++ Hacks/hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Nathan Hartman
+ * Date: 2025/08/27
+ * 
+ * Prints my Name
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Nathan Hartman");
+
+    return 0;
+}
